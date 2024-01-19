37b5fb3ad9464716231c8ab059969dfa23a88317
Joni Louhela <louhela.joni@gmail.com>
2024-01-19 20:04:16 +0200

    Deck to interface target, use in gtest

---
+++ src/include/deck/deck.h
@@ -0,0 +1,9 @@
+template <typename T>
+class Deck
+{
+public:
+    int test()
+    {
+        return 0;
+    }
+};
--- test/src/deck_test.cpp
+++ test/src/deck_test.cpp
@@ -1,10 +1,9 @@
+#include "deck/deck.h"
+
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(DeckTest, FirstTest)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    Deck<int> deck;
+    EXPECT_EQ(0, deck.test());
 }
